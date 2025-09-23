c51902ebc434ffebd8d7fb4ebf5759f515b2d963
Anastasiia Fomkina <anastasiia.fomkina@masabi.com>
2025-09-23 13:06:40 -0300

    Test commit

---
+++ src/main/java/ru/practicum/jenkins/example/JenkinsExampleApplication.java
@@ -0,0 +1,11 @@
+package ru.practicum.jenkins.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsExampleApplication.class, args);
+    }
+}
---
+++ src/main/java/ru/practicum/jenkins/example/UserController.java
@@ -0,0 +1,42 @@
+package ru.practicum.jenkins.example;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final Map<Long, UserDto> users = new ConcurrentHashMap<>();
+    private final AtomicLong idGenerator = new AtomicLong(1);
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
+        UserDto userDto = users.get(id);
+        if (userDto != null) {
+            return ResponseEntity.ok(userDto);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+        long id = idGenerator.getAndIncrement();
+        userDto.setId(id);
+        users.put(id, userDto);
+        URI location = URI.create("/users/" + id);
+        return ResponseEntity.created(location).body(userDto);
+    }
+}
+
